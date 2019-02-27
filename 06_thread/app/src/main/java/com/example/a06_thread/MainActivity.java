be3c60b8fad56ea69c4889d5afff51dde71071ec
yoojh9 <yoojh9@gmail.com>
2019-02-27 17:16:21 +0900

    06_thread 프로젝트 생성

---
+++ 06_thread/app/src/main/java/com/example/a06_thread/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.a06_thread;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnStart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
