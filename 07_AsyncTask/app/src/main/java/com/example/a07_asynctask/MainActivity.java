36cc621a469757b8acf486b9900b4cc2ec8af86b
yoojh9 <yoojh9@gmail.com>
2019-02-27 18:13:10 +0900

    07_AsyncTask 코드 추가

--- 07_AsyncTask/app/src/main/java/com/example/a07_asynctask/MainActivity.java
+++ 07_AsyncTask/app/src/main/java/com/example/a07_asynctask/MainActivity.java
@@ -23,11 +23,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         MyTask myTask = new MyTask();
-        myTask.execute();
+        myTask.execute(30,40); // parameter 추가할 수 있음
     }
 
 
-    class MyTask extends AsyncTask<Void, Integer, Void> {
+    class MyTask extends AsyncTask<Integer, Integer, String> {
 
         // Integer ... : 가변인자 / 배열
         @Override
@@ -35,10 +35,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             btnStart.setText("count: " + values[0]);
         }
 
-        // 백그라운드에서 어떤 작업을 할지 (Thread 영역에 해당 - myThread의 run()부분과 동일)
+
         @Override
-        protected Void doInBackground(Void... voids) {
-            for(int i=0; i<=100; i++){
+        protected void onPostExecute(String s) {
+            btnStart.setText(s);
+        }
+
+        // 백그라운드에서 어떤 작업을 할지 (Thread 영역에 해당 - myThread의 run()부분과 동일)
+        // doInBackground(파라미터로 받은 값)
+        @Override
+        protected String doInBackground(Integer... values) {
+            for(int i=values[0]; i<=100; i++){
                 Log.d("myTask", "count : "+ i);
                 publishProgress(i);  // 중간에 값을 넘김 -> onProgressUpdate() 에서 데이터를 받음
                 // 가변인자 publishProgress(1,2,3,4) 이렇게 보낼 수 있음.
@@ -49,7 +56,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     e.printStackTrace();
                 }
             }
-            return null;
+            return "count done!!";    // onPostExecute()에서 처리
         }
 
 
