1c1a263f2a78944b6f733bdf1a19a022bc9882c8
yoojh9 <yoojh9@gmail.com>
2019-02-27 15:49:33 +0900

    04_customListView 데이터 생성

--- 04_customListView/app/src/main/java/com/example/a04_customlistview/MainActivity.java
+++ 04_customListView/app/src/main/java/com/example/a04_customlistview/MainActivity.java
@@ -3,11 +3,24 @@ package com.example.a04_customlistview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<MyData> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addMyData();
+    }
+
+    private void addMyData(){
+        for(int i=0; i<100; i++){
+            int iconId = (i%2==0)?R.mipmap.ic_launcher : R.mipmap.ic_launcher_round;
+            list.add(new MyData("title"+i, "desc"+i, iconId));
+        }
     }
 }
---
+++ 04_customListView/app/src/main/java/com/example/a04_customlistview/MyData.java
@@ -0,0 +1,15 @@
+package com.example.a04_customlistview;
+
+public class MyData {
+    String title;
+    String description;
+    int imgIcon;
+
+    public MyData(String title, String description, int imgIcon) {
+        this.title = title;
+        this.description = description;
+        this.imgIcon = imgIcon;
+    }
+
+    public MyData(){}
+}
