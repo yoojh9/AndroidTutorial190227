acf9c0ef495e4cd32fc723a2fcf64abb3ab2284a
yoojh9 <yoojh9@gmail.com>
2019-02-27 15:23:32 +0900

    03_ListView 실습 추가

--- 03_ListView/app/src/main/java/com/example/a03_listview/MainActivity.java
+++ 03_ListView/app/src/main/java/com/example/a03_listview/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.a03_listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] list = {"hello", "world", "oracle", "java",
+            "hello", "world", "oracle", "java",
+            "hello", "world", "oracle", "java",
+            "hello", "world", "oracle", "java"};
+
+    ListView listView;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+
+        adapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, // 레이아웃을 따로 안만들고 내장된 레이아웃을 사용함
+                list);
+        listView.setAdapter(adapter);
+
     }
 }
