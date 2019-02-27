ed1290ce996c7d2c48e9762515ea1cdea9af1cbf
yoojh9 <yoojh9@gmail.com>
2019-02-27 15:11:02 +0900

    02_Activity 실습 내용 추가

--- 02_Activity/app/src/main/java/com/example/a02_activity/MainActivity.java
+++ 02_Activity/app/src/main/java/com/example/a02_activity/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.a02_activity;
 
+import android.content.Intent;
+import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_REQ_CODE = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnStart = findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 액티비티 호출 (명시적 인텐트)
+                Intent intent = new Intent(MainActivity.this, MyActivity.class);
+                intent.putExtra("myValue", "Hello World");
+                intent.putExtra("intValue", 100);
+                startActivityForResult(intent, MY_REQ_CODE); // requestCode: request 요청 구분 용
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+       if(requestCode == MY_REQ_CODE && resultCode == RESULT_OK){
+           String str = data.getStringExtra("myResult");
+           Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+       }
     }
 }
--- 02_Activity/app/src/main/java/com/example/a02_activity/MyActivity.java
+++ 02_Activity/app/src/main/java/com/example/a02_activity/MyActivity.java
@@ -1,7 +1,11 @@
 package com.example.a02_activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MyActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class MyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+
+        Intent intent = getIntent(); // MainActivity에서 전달한 인텐트를 받아올 수 있음
+        String value = intent.getStringExtra("myValue");
+        int intValue = intent.getIntExtra("intValue", 0);   // defaultValue 설정 안해주면 오류 남
+        Toast.makeText(MyActivity.this, "myValue: " + value + ", intValue: "+ intValue, Toast.LENGTH_SHORT).show();
+
+        Button btnRes = findViewById(R.id.btnRes);
+        btnRes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent resIntent = new Intent();
+                resIntent.putExtra("myResult", "MY RESULT!");
+                setResult(RESULT_OK, resIntent);    // 자기를 호출한 액티비티에 결과값을 보내거나 추가적인 데이터를 보낼 수 있다. 역시 인텐트를 이용해야 함
+                finish(); // 종료
+            }
+        });
     }
 }
