710b40cc5c867541817190ec5169933a4b60e554
yoojh9 <yoojh9@gmail.com>
2019-02-27 12:04:20 +0900

    event listener

--- 01_Widget/app/src/main/java/com/example/a01_widget/MainActivity.java
+++ 01_Widget/app/src/main/java/com/example/a01_widget/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.a01_widget;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        Button btnHello = findViewById(R.id.btnHello);
+        btnHello.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Button Clicked");
+            }
+        });
+
+        CheckBox checkBox = findViewById(R.id.checkBox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                    Toast.makeText(MainActivity.this, "checked", Toast.LENGTH_SHORT).show();
+                    Log.d(TAG, "check box is checked");
+                } else {
+                    Log.d(TAG, "check box is unChecked");
+                }
+            }
+        });
     }
 }
