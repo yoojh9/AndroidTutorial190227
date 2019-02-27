3b1c37d342c42835575b5fb1425ab7b1e75db3cd
yoojh9 <yoojh9@gmail.com>
2019-02-27 15:38:57 +0900

    03_ListView setOnItemClickListener() 추가

--- 03_ListView/app/src/main/java/com/example/a03_listview/MainActivity.java
+++ 03_ListView/app/src/main/java/com/example/a03_listview/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.a03_listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +28,12 @@ public class MainActivity extends AppCompatActivity {
                 android.R.layout.simple_list_item_1, // 레이아웃을 따로 안만들고 내장된 레이아웃을 사용함
                 list);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+                Toast.makeText(MainActivity.this, position + "번째 값:" + list[position], Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
