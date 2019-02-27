4430652b00b969b4a188dc2e16eca172a575e590
yoojh9 <yoojh9@gmail.com>
2019-02-27 17:48:42 +0900

    06_thread progressBar 예제 추가

--- 06_thread/app/src/main/java/com/example/a06_thread/MainActivity.java
+++ 06_thread/app/src/main/java/com/example/a06_thread/MainActivity.java
@@ -7,11 +7,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final int MY_COUNT = 1;
     Button btnStart;
+    ProgressBar progressBar;
 
     Handler handler = new Handler(){
         // sendMessage() 해놓으면 킵해놓고 있다가 처리할 수 있는 상황이 오면 handleMessage()를 이용해 처리
@@ -19,14 +21,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         public void handleMessage(Message msg) {
             if(msg.what == MY_COUNT){
                 btnStart.setText("count : " + msg.arg1);
+                progressBar.setProgress(msg.arg1);
             }
         }
     };
 
+    // Logical한 코드만 쓰레드에 넣고, UI 조작 관련 코드는 Handler에 넣는 것을 권장함
     class MyThread extends Thread {
         @Override
         public void run() {
-            for(int i=0; i<100; i++){
+            for(int i=0; i<=100; i++){
                 Log.d("thread", "count: " + i);
 
                 Message msg = new Message();
@@ -53,6 +57,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnStart = findViewById(R.id.btnStart);
         btnStart.setOnClickListener(this);
+
+        progressBar = findViewById(R.id.progressBar);
     }
 
     @Override
