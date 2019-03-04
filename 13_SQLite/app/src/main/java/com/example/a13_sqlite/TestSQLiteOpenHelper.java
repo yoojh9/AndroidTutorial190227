643661414b209ee9e07a2702c422cf2d8ded9ec8
yoojh9 <yoojh9@gmail.com>
2019-03-04 14:03:02 +0900

    add 13_SQLite

---
+++ 13_SQLite/app/src/main/java/com/example/a13_sqlite/TestSQLiteOpenHelper.java
@@ -0,0 +1,24 @@
+package com.example.a13_sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class TestSQLiteOpenHelper extends SQLiteOpenHelper {
+
+    public TestSQLiteOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE student (id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "name TEXT , age INTEGER, address TEXT);";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
