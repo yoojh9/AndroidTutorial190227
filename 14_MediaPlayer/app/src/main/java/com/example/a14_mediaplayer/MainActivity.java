d776408516c0bc95cda34d8455b480821997306e
yoojh9 <yoojh9@gmail.com>
2019-03-04 17:25:03 +0900

    external storage path 변경(emulator 실행 용)

--- 14_MediaPlayer/app/src/main/java/com/example/a14_mediaplayer/MainActivity.java
+++ 14_MediaPlayer/app/src/main/java/com/example/a14_mediaplayer/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.media.MediaPlayer;
+import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -59,15 +60,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setupPermission();
 
         findViewById(R.id.btnPlay).setOnClickListener(this);
+        findViewById(R.id.btnPlayFile).setOnClickListener(this);
         findViewById(R.id.btnStop).setOnClickListener(this);
+    }
+
+
+    private String getPath(int id){
+        if(id==R.id.btnPlay) {
+            return "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+        }
+        else if(id==R.id.btnPlayFile) {
+            // 디바이스 내 모든 앱들이 공통적으로 접근할 수 있는 디렉토리  /storage/emulated/0/Kalimba.mp3 ??
+            Log.i("file ", Environment.getExternalStorageDirectory()+"/Kalimba.mp3");
+
+            // 아래 코드로 해야지 되네..
+            return "/sdcard/Download/Kalimba.mp3";
+        } else {
+            return null;
+        }
     }
 
     @Override
     public void onClick(View v) {
-        if(v.getId()==R.id.btnPlay){
+
+        if(v.getId()==R.id.btnPlay || v.getId() == R.id.btnPlayFile){
             mp = new MediaPlayer();
             try {
-                mp.setDataSource("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4");
+
+                mp.setDataSource(getPath(v.getId()));
                 mp.prepare();
                 // prepare를 하고 나면 mp.getDuration()으로 총 재생시간을 알 수 있음.
                 mp.start();
