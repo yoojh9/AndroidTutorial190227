2f879858eefbfa1571c58a1b60352ae01acc01cc
yoojh9 <yoojh9@gmail.com>
2019-03-04 14:52:08 +0900

    13_SQLite 예제 코드 추가

--- 13_SQLite/app/src/main/java/com/example/a13_sqlite/MainActivity.java
+++ 13_SQLite/app/src/main/java/com/example/a13_sqlite/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.a13_sqlite;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TestDBHandler dbHandler = new TestDBHandler(this);
+        dbHandler.insert("Jeonghyun", 30, "서울시");
+        dbHandler.insert("Yong", 31, "서울시");
+        dbHandler.insert("정현", 30, "인천시");
+
+        dbHandler.update("정현", 29);
+
+        String res = dbHandler.selectAll();
+        Log.i("SQLite : " , res);
     }
 }
--- 13_SQLite/app/src/main/java/com/example/a13_sqlite/TestDBHandler.java
+++ 13_SQLite/app/src/main/java/com/example/a13_sqlite/TestDBHandler.java
@@ -1,11 +1,52 @@
 package com.example.a13_sqlite;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 public class TestDBHandler {
     TestSQLiteOpenHelper helper;
 
     public TestDBHandler(Context context) {
         helper = new TestSQLiteOpenHelper(context, "people.db", null, 1);
+    }
+
+    public void insert(String name, int age, String address){
+       SQLiteDatabase db = helper.getWritableDatabase();
+       ContentValues values = new ContentValues();
+       values.put("name", name);
+       values.put("age", age);
+       values.put("address", address);
+       db.insert("student", null, values);
+    }
+
+    public void delete(String name){
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.delete("student", "name = ?", new String[]{name});
+        // db.delete("student", "name = ? and address = ?", new String[]{name, address});
+    }
+
+    public void update(String name, int newAge){
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("age", newAge);
+        db.update("student", values, "name = ?", new String[]{name});
+    }
+
+    public String selectAll(){
+        String res = "";
+        SQLiteDatabase db = helper.getReadableDatabase();
+        Cursor c = db.query("student", null, null, null, null, null, null);
+
+        while(c.moveToNext()){
+            String name = c.getString(c.getColumnIndex("name"));
+            int age = c.getInt(c.getColumnIndex("age"));
+            String address = c.getString(c.getColumnIndex("address"));
+
+            res += "name : " + name + " , age : " + age + ", address : " + address + "\n";
+        }
+
+        return res;
     }
 }
