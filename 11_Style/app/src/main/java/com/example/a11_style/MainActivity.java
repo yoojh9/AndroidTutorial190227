098cc4fec711bdfccf5129a2dfd541a24a6a296b
yoojh9 <yoojh9@gmail.com>
2019-03-04 11:07:03 +0900

    버튼 이벤트 추가

--- 11_Style/app/src/main/java/com/example/a11_style/MainActivity.java
+++ 11_Style/app/src/main/java/com/example/a11_style/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.a11_style;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +17,35 @@ public class MainActivity extends AppCompatActivity {
 
         selectedTextView = findViewById(R.id.selectedTextView);
         workingTextView = findViewById(R.id.workingTextView);
+
+        View.OnClickListener numberListener = new View.OnClickListener() {
+            // View는 버튼에 붙으면 버튼이 됨
+            @Override
+            public void onClick(View v) {
+                String str = ((Button) v).getText().toString();
+                String working = workingTextView.getText().toString();
+                if(working.equals("0")){
+                    workingTextView.setText(str);
+                } else {
+                    workingTextView.append(str);
+                }
+
+            }
+        };
+
+        Button zeroButton = findViewById(R.id.zeroButton);
+        zeroButton.setOnClickListener(numberListener);
+        Button oneButton = findViewById(R.id.oneButton);
+        oneButton.setOnClickListener(numberListener);
+
+        Button enterButton = findViewById(R.id.enterButton);
+        enterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = workingTextView.getText().toString();
+                workingTextView.setText("0");
+                selectedTextView.setText(str);
+            }
+        });
     }
 }
