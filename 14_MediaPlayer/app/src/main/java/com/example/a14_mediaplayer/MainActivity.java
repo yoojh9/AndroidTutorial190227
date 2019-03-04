94686036d86c834ded956bd48f45d4e7fe63d488
yoojh9 <yoojh9@gmail.com>
2019-03-04 15:24:58 +0900

    Runtime 퍼미션 추가

--- 14_MediaPlayer/app/src/main/java/com/example/a14_mediaplayer/MainActivity.java
+++ 14_MediaPlayer/app/src/main/java/com/example/a14_mediaplayer/MainActivity.java
@@ -1,13 +1,47 @@
 package com.example.a14_mediaplayer;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.security.Permissions;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQ_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupPermission();
+    }
+
+    // Runtime Permission 얻는 과정
+    private void setupPermission(){
+       int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        // 권한 부여받지 않았을 경우 권한 요청청
+       if(permission != PackageManager.PERMISSION_GRANTED){
+           ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_REQ_CODE);
+        } else {
+           Log.i("Permission", "already granted");
+       }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == PERMISSION_REQ_CODE){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Log.i("Permission", "granted");
+            } else {
+                Log.i("Permission", "not granted");
+            }
+        }
     }
 }
