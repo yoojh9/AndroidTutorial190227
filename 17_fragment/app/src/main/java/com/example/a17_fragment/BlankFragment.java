bef811aea0022ca2369ffd2dc273cc512f895a17
yoojh9 <yoojh9@gmail.com>
2019-03-05 11:07:17 +0900

    17_Fragment add

---
+++ 17_fragment/app/src/main/java/com/example/a17_fragment/BlankFragment.java
@@ -0,0 +1,24 @@
+package com.example.a17_fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_blank, container, false);
+        return v;
+    }
+
+}
