17bc9d602df085ca034c085b424d7bff2197914c
yoojh9 <yoojh9@gmail.com>
2019-03-05 17:21:18 +0900

    20_CustomView add

--- 20_CustomView/app/src/main/java/com/example/a20_customview/Box.java
+++ 20_CustomView/app/src/main/java/com/example/a20_customview/Box.java
@@ -1,4 +1,29 @@
 package com.example.a20_customview;
 
+import android.graphics.PointF;
+
 public class Box {
+
+    private PointF current;
+    private PointF origin;
+
+    public Box(PointF origin) {
+        this.origin = origin;
+    }
+
+    public PointF getCurrent() {
+        return current;
+    }
+
+    public PointF getOrigin() {
+        return origin;
+    }
+
+    public void setCurrent(PointF current) {
+        this.current = current;
+    }
+
+    public void setOrigin(PointF origin) {
+        this.origin = origin;
+    }
 }
--- 20_CustomView/app/src/main/java/com/example/a20_customview/BoxDrawingView.java
+++ 20_CustomView/app/src/main/java/com/example/a20_customview/BoxDrawingView.java
@@ -1,4 +1,75 @@
 package com.example.a20_customview;
 
-public class BoxDrawingView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BoxDrawingView extends View {
+    private static final String TAG = "BoxDrawingView";
+    private Box currentBox;
+    private List<Box> mBoxes = new ArrayList<>();
+    private Paint boxPaint;
+    private Paint backgroundPaint;
+
+    // code
+    public BoxDrawingView(Context context) {
+        super(context);
+    }
+
+    // xml
+    public BoxDrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        boxPaint = new Paint();
+        boxPaint.setColor(0x22ff0000);
+
+        backgroundPaint = new Paint();
+        backgroundPaint.setColor(0xfff8efe0);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        PointF current = new PointF(event.getX(), event.getY());
+
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                currentBox = new Box(current);
+                mBoxes.add(currentBox);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if(currentBox != null){
+                    currentBox.setCurrent(current);
+                    invalidate();
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                currentBox = null;
+                break;
+        }
+        Log.d(TAG, "action : "+event.getAction() +
+                " x : "+current.x + " y : "+current.y);
+
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawPaint(backgroundPaint);
+
+        for(Box box : mBoxes){
+            float left = Math.min(box.getOrigin().x, box.getCurrent().x);
+            float right = Math.max(box.getOrigin().x, box.getCurrent().x);
+            float top = Math.min(box.getOrigin().y, box.getCurrent().y);
+            float bottom = Math.max(box.getOrigin().y, box.getCurrent().y);
+
+            canvas.drawRect(left, top, right,bottom, boxPaint);
+        }
+    }
 }
